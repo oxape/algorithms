6d2b16c275f35ab3016e3cd2a637c1cdeb33b330
oxape <wxdeve@126.com>
2020-04-22 23:29:43 +0800

    fix binary search

--- ch1/binary-search.py
+++ ch1/binary-search.py
@@ -4,20 +4,17 @@ sorted_list = [1, 2, 4, 5, 9, 10, 20, 23]
 def binary_searcy(n, sorted_list):
     low = 0
     high = len(sorted_list)-1
-    while low < high:
+    while low <= high:
         middle = (low+high)//2
         if n > sorted_list[middle]:
-            low = middle
+            low = middle+1
         elif n < sorted_list[middle]:
-            high = middle
+            high = middle-1
         else:
             return middle
-    if low == n:
-        return low
     return None
 
 if __name__ == "__main__":
-    result = binary_searcy(23, sorted_list)
     for e in sorted_list:
         result = binary_searcy(e, sorted_list)
         print(result)
