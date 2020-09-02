b46fbf0b7cd075d8a24c9224bba85afa0914a6cf
donglu <donglu@starot.com>
2020-09-02 13:53:55 +0800

    optimize ch7/dijkstra.py

--- ch7/dijkstra.py
+++ ch7/dijkstra.py
@@ -1,12 +1,25 @@
 
 
-def find_lowest_cost_node(costs: dict, processed: list) -> str:
+def find_lowest_cost_node(costs: dict, processed: set) -> str:
     lowest_cost = float('inf')
     lowest_cost_node = None
+    '''
     for node in costs:
         cost = costs[node]
         # 如果当前节点的开销更低且未处理过
         if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    '''
+    # 优化循环 这个优化其实应该在这个函数外面做，这样就不用反复创建q了，这里只做演示用途，正确的做法是创建一个空的q之后，添加元素到costs同时添加到q，添加元素到processed时同时从q删除
+    q = set(costs.keys())
+    for e in processed:
+        q.remove(e)
+    
+    for node in q:
+        cost = costs[node]
+        # 如果当前节点的开销更低且未处理过
+        if cost < lowest_cost:
             lowest_cost = cost
             lowest_cost_node = node
     return lowest_cost_node
@@ -15,13 +28,17 @@ def find_lowest_cost_node(costs: dict, processed: list) -> str:
 def find_lowest_path(graph: dict, start: str, end):
     parents = dict()
     costs = dict()
+    '''
     infinity = float('inf')
     for k, v in graph[start].items():
         costs[k] = v
         parents[k] = start
     parents[end] = None
     costs['fin'] = infinity
-    processed = []
+    '''
+    # 上面的代码优化为如下
+    costs[start] = 0
+    processed = set()
     while True:
         # 在未处理的节点中找出开销最小的节点
         node = find_lowest_cost_node(costs, processed)
@@ -37,7 +54,7 @@ def find_lowest_path(graph: dict, start: str, end):
             if n not in costs or costs[n] > new_cost:
                 costs[n] = new_cost
                 parents[n] = node
-        processed.append(node)
+        processed.add(node)
     print(costs)
     parent = end
     path = [end]
