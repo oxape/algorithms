c5ef32dfa512fb62e867c6e8af9af698390a23b0
donglu <donglu@starot.com>
2022-08-03 20:54:53 +0800

    b-tree unfinished

--- Algorithms/b-tree.py
+++ Algorithms/b-tree.py
@@ -1,3 +1,4 @@
+from typing import *
 from graphviz import Graph
 
 def swap(arr, a, b):
@@ -22,11 +23,37 @@ def quick_sort_recursive(arr, start, end):
     quick_sort_recursive(arr, start, pivot_index-1)
     quick_sort_recursive(arr, pivot_index+1, end)
 
+class Index:
+    def __init__(self, key, value):
+        self._key = key
+        self._value = value
+    
+    @property
+    def key(self):
+        return self._key
 
-# class Node(object):
-#     def __init__(self, value) -> None:
-#         self._children = OrderedDict()
-#         self._value = value
+    @property
+    def value(self):
+        return self._value
+
+class Node:
+    def __init__(self, max):
+        self.max = max
+        self.size = 0
+        self.indexs = []
+        self.pointers = []
+
+class BTree:
+    def __init__(self, rank):
+        self.rank = rank
+        self.root = Node(rank)
+
+    def insert(self, node: Node):
+        
+
+    def __insert(self, parent):
+    
+
 if __name__ == '__main__':
     arr = [10, 12, 3, 1, 15, 5, 4, 20, 21, 18, 9, 10, 33, 17]
     num = len(arr)
