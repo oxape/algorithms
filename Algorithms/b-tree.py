84b9e3ec6467307d73e11e84579c9e1af6d4caf9
donglu <donglu@starot.com>
2022-08-05 18:36:26 +0800

    b-tree continue

--- Algorithms/b-tree.py
+++ Algorithms/b-tree.py
@@ -1,3 +1,4 @@
+from turtle import right
 from typing import *
 from graphviz import Digraph, nohtml
 from operator import itemgetter
@@ -74,47 +75,70 @@ class Item:
         return f'{{{self.key}:{self.value}}}'
 
 class Node:
-    def __init__(self):
+    def __init__(self, isLeaf):
+        self.isLeaf = isLeaf
         self.items = []
-        self.pointers = []
+        self.pointers = [None]
         self.parent = None
+        self.posInParent = 0
 
     @property
     def size(self):
         return len(self.items)
-
-    @property
-    def isLeaf(self):
-        return len(self.pointers) == 0
 
     def insert(self, item):
         pos = binary_search_to_insert(self.items, item, itemgetter('key'))
         if pos < self.size:
             prevSize = self.size
             self.items.append(None)
+            self.pointers.append(None)
             for i in reversed(range(pos, prevSize)):
                 self.items[i+1] = self.items[i]
+                self.pointers[i+1] = self.items[i]
             self.items[pos] = item
         else:
             self.items.append(item)
+            self.pointers.append(None)
 
 class BTree:
     def __init__(self, rank):
         self.rank = rank
-        self.root = Node()
+        self.root = Node(True)
 
     def insert(self, item: Item):
         node = self._find_node_to_insert(self.root, item)
-        if node.size < self.rank-1:
-            node.insert(item)
+        node.insert(item)
+        if node.size <= self.rank-1:
             return
-        pos = binary_search_to_insert(node.items, item, itemgetter('key'))
-        print(pos)
+        middle = node.size//2
+        leftChild = Node(True)
+        leftChild.items = node.items[:middle]
+        rightChild = Node(True)
+        rightChild.items = node.items[middle+1:]
+        while True:
+            pos = node.posInParent
+            parent = node.parent
+            if parent is None:
+                parent = Node(False)
+            prevSize = parent.size
+            parent.items.append(None)
+            parent.pointers.append(None)
+            for i in reversed(range(pos, prevSize)):
+                parent.items[i+1] = self.items[i]
+                parent.pointers[i+1] = self.pointers[i]
+            parent.items[pos] = item
+            parent.pointers[pos] = leftChild
+            parent.pointers[pos+1] = rightChild
+            if 
+            if parent.size < self.rank-1:
+                break
+            else:
+                break
 
     def _find_node_to_insert(self, node: Node, item: Item):
-        if len(node.pointers) == 0:
+        if node.isLeaf:
             return node
-        items_len = node.items
+        items_len =  node.size
         for i in range(items_len):
             if item.key < node.items[i].key:
                 return node.pointers[i]
@@ -122,10 +146,10 @@ class BTree:
 
     def _insert(self, parent):
         pass
-    
+
 
 if __name__ == '__main__':
-    arr = [Item(13, 0), Item(5, 1), Item(2, 1), Item(5, 1), Item(6, 1)]
+    arr = [Item(13, 0), Item(5, 1), Item(2, 1), Item(7, 1), Item(6, 1)]
 
     btree = BTree(5)
     for item in arr:
